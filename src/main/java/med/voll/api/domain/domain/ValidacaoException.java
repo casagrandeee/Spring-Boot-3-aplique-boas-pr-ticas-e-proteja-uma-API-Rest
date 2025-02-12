48a2fd07b6b813739ab2124c7edd3e59b864e75c
casagrandeee <110943883+casagrandeee@users.noreply.github.com>
2025-02-12 19:23:59 -0300

    Dia 01 do curso

---
+++ src/main/java/med/voll/api/domain/domain/ValidacaoException.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.domain;
+
+public class ValidacaoException extends RuntimeException {
+
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
